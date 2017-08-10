2ea69fa0249c252f55891b239dcfa3644dc4c11e
Jonas Carpay <jonascarpay@gmail.com>
2017-08-10 17:17:31 +0200

    Simplemap component

--- src/Control/ECS/Immutable.hs
+++ src/Control/ECS/Immutable.hs
@@ -2,3 +2,16 @@ module Control.ECS.Immutable where
 
 import qualified Data.IntSet as S
 import qualified Data.IntMap as M
+
+import Control.ECS.Types
+
+newtype SimpleMap c = SimpleMap c
+
+instance Component (SimpleMap c) where
+  type Repr    (SimpleMap c) = Maybe c
+  type Storage (SimpleMap c) = M.IntMap c
+  empty = Store mempty
+  slice (Store m) = return (Slice (M.keysSet m))
+  retrieve (Entity e) (Store m) = return (Reads (M.lookup e m))
+  store (Entity e) (Writes (Just c)) (Store m) = return (Store (M.insert e c m))
+  store (Entity e) (Writes Nothing) (Store m) = return (Store (M.delete e m))
