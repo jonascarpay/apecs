a0ba6124c779bb5112cb64a95dbe6dadb66c809a
Jonas Carpay <jonascarpay@gmail.com>
2017-08-10 19:02:08 +0200

    Upgrade to ghc 8.2.1

--- src/Control/ECS.hs
+++ src/Control/ECS.hs
@@ -1,13 +1,14 @@
 module Control.ECS (
   module Control.ECS.Types,
-  module Control.ECS.Instances,
+  module Control.ECS,
 ) where
 
 import Control.ECS.Types
-import Control.ECS.Instances
 
 import qualified Data.IntSet as S
 
 union :: Slice s1 -> Slice s2 -> Slice ()
 union (Slice s1) (Slice s2) = Slice (s1 `S.union` s2)
 
+toList :: Slice c -> [Entity]
+toList (Slice s) = fmap Entity (S.toList s)
--- src/Main.hs
+++ src/Main.hs
@@ -1,13 +1,16 @@
 import Control.ECS
+import Control.ECS.Immutable
 
-data Vec2        = Vec Float Float deriving (Eq, Show)
-newtype Position = Pos Vec2 deriving (Eq, Show)
-newtype Velocity = Vel Vec2 deriving (Eq, Show)
-data Enemy       = Enemy deriving (Eq, Show)
-{-data MyWld       = MyWld { positions  :: !(M.IntMap Position)-}
-                         {-, velocities :: !(M.IntMap Velocity)-}
-                         {-, enemies    :: !S.IntSet-}
-                         {-, eCounter   :: Int-}
-                         {-} deriving (Eq, Show)-}
+data V2 = V2 !Float !Float
+newtype Position = Position (SimpleMap V2)
+newtype Velocity = Velocity (SimpleMap V2)
+newtype IsEnemy  = IsEnemy SimpleFlag
+
+data World = World
+  { positions     :: Storage Position
+  , velocities    :: Storage Velocity
+  , enemies       :: Storage IsEnemy
+  , entityCounter :: Storage EntityCounter
+  }
 
 main = undefined
