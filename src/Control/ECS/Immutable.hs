99595d36fd1d674b00783a5e3ad3d5219a3e4fa9
Jonas Carpay <jonascarpay@gmail.com>
2017-08-11 18:42:23 +0200

    Monad transformer rewrite

--- src/Control/ECS/Immutable.hs
+++ src/Control/ECS/Immutable.hs
@@ -4,36 +4,47 @@ module Control.ECS.Immutable where
 
 import qualified Data.IntSet as S
 import qualified Data.IntMap as M
+import Control.Monad.State
+import Control.Lens
 
 import Control.ECS.Types
 
-{-
 newtype SimpleMap c = SimpleMap c deriving (Eq, Show)
 instance Component (SimpleMap c) where
+
   type Repr    (SimpleMap c) = Maybe c
   type Storage (SimpleMap c) = M.IntMap c
+
   empty = Store mempty
-  slice (Store s)                              = return (Slice (M.keysSet s))
-  retrieve (Entity e) (Store s)                = return (Reads (M.lookup e s))
-  store (Entity e) (Writes (Just c)) (Store s) = return (Store (M.insert e c s))
-  store (Entity e) (Writes Nothing) (Store s)  = return (Store (M.delete e s))
+
+  slice = Slice . M.keysSet . _unStore <$> get
+  retrieve (Entity e) = Reads . M.lookup e . _unStore <$> get
+
+  store (Entity e) (Writes (Just x)) = unStore %= M.insert e x
+  store (Entity e) (Writes Nothing)  = unStore %= M.delete e
 
 data SimpleFlag
 instance Component SimpleFlag where
+
   type Repr    SimpleFlag = Bool
   type Storage SimpleFlag = S.IntSet
+
   empty = Store mempty
-  slice (Store s)                           = return (Slice s)
-  retrieve (Entity e) (Store s)             = return (Reads (S.member e s))
-  store (Entity e) (Writes True)  (Store s) = return (Store (S.insert e s))
-  store (Entity e) (Writes False) (Store s) = return (Store (S.delete e s))
+
+  slice = Slice . _unStore <$> get
+  retrieve (Entity e) = Reads . S.member e . _unStore <$> get
+
+  store (Entity e) (Writes True) = unStore %= S.insert e
+  store (Entity e) (Writes True) = unStore %= S.delete e
 
 data EntityCounter
 instance Component EntityCounter where
-  type Repr EntityCounter = Int
+
+  type Repr    EntityCounter = Int
   type Storage EntityCounter = Int
+
   empty = Store 0
-  slice _ = return (Slice (S.singleton (-1)))
-  retrieve _ (Store ix) = return (Reads ix)
-  store _ (Writes ix) _ = return (Store ix)
-  -}
+
+  slice = return . Slice . S.singleton $ -1
+  retrieve _ = Reads . _unStore <$> get
+  store _ (Writes count) = unStore .= count
--- src/Control/ECS/Types.hs
+++ src/Control/ECS/Types.hs
@@ -18,10 +18,11 @@ class Component comp where
   retrieve :: Entity -> System (Store comp) (Reads comp)
   store    :: Entity -> Writes comp -> System (Store comp) ()
 
-newtype System s a = System { runSystem :: StateT s IO a }
+newtype System s a = System {runSystem :: StateT s IO a}
 deriving instance Functor      (System s)
 deriving instance Applicative  (System s)
 deriving instance Monad        (System s)
+deriving instance MonadIO      (System s)
 deriving instance MonadState s (System s)
 
 newtype Slice  comp = Slice S.IntSet
@@ -29,5 +30,29 @@ newtype Reads  comp = Reads (Repr comp)
 newtype Writes comp = Writes (Repr comp)
 newtype Store  comp = Store { _unStore :: Storage comp }
 makeLenses ''Store
+makeWrapped ''Store
 
+runSystem' :: System s a -> s -> System c (a, s)
+runSystem' sys = liftIO . runStateT (runSystem sys)
 
+instance (Component a, Component b) => Component (a, b) where
+
+  type Repr    (a, b) = (Repr a, Repr b)
+  type Storage (a, b) = (Storage a, Storage b)
+
+  empty =
+    let Store sta :: Store a = empty
+        Store stb :: Store b = empty
+     in Store (sta, stb)
+
+  slice =
+    do Store (sta, stb) <- get
+       (Slice sla, Store sta') <- runSystem' slice (Store sta :: Store a)
+       (Slice slb, Store stb') <- liftIO . runStateT (runSystem slice) $ (Store stb :: Store b)
+       put (Store (sta', stb'))
+       return $ Slice (sla `S.intersection` slb)
+
+  retrieve ety =
+    do Store (sta, stb) <- get
+       (Reads ra, Store sta') <- liftIO . runStateT (runSystem $ retrieve ety) $ (Store sta :: Store a)
+       undefined
