51db73bbda5433575803b7cad1cc7bd066fc389a
Jonas Carpay <jonascarpay@gmail.com>
2017-08-11 19:38:54 +0200

    Eplicit constraints

--- src/Control/ECS/Immutable.hs
+++ src/Control/ECS/Immutable.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE MultiParamTypeClasses, FlexibleInstances, TypeFamilies #-}
 
 module Control.ECS.Immutable where
 
@@ -10,12 +10,12 @@ import Control.Lens
 import Control.ECS.Types
 
 newtype SimpleMap c = SimpleMap c deriving (Eq, Show)
-instance Component (SimpleMap c) where
+instance Monad m => Component m (SimpleMap c) where
 
   type Repr    (SimpleMap c) = Maybe c
   type Storage (SimpleMap c) = M.IntMap c
 
-  empty = Store mempty
+  empty = return $ Store mempty
 
   slice = Slice . M.keysSet . _unStore <$> get
   retrieve (Entity e) = Reads . M.lookup e . _unStore <$> get
@@ -24,12 +24,12 @@ instance Component (SimpleMap c) where
   store (Entity e) (Writes Nothing)  = unStore %= M.delete e
 
 data SimpleFlag
-instance Component SimpleFlag where
+instance Monad m => Component m SimpleFlag where
 
   type Repr    SimpleFlag = Bool
   type Storage SimpleFlag = S.IntSet
 
-  empty = Store mempty
+  empty = return $ Store mempty
 
   slice = Slice . _unStore <$> get
   retrieve (Entity e) = Reads . S.member e . _unStore <$> get
@@ -38,12 +38,12 @@ instance Component SimpleFlag where
   store (Entity e) (Writes True) = unStore %= S.delete e
 
 data EntityCounter
-instance Component EntityCounter where
+instance Monad m => Component m EntityCounter where
 
   type Repr    EntityCounter = Int
   type Storage EntityCounter = Int
 
-  empty = Store 0
+  empty = return $ Store 0
 
   slice = return . Slice . S.singleton $ -1
   retrieve _ = Reads . _unStore <$> get
--- src/Control/ECS/Types.hs
+++ src/Control/ECS/Types.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE StandaloneDeriving, TemplateHaskell, TypeFamilies, MultiParamTypeClasses, GeneralizedNewtypeDeriving, ScopedTypeVariables, TypeOperators, FlexibleInstances #-}
+{-# LANGUAGE ConstraintKinds, FlexibleContexts, StandaloneDeriving, TemplateHaskell, TypeFamilies, MultiParamTypeClasses, GeneralizedNewtypeDeriving, ScopedTypeVariables, TypeOperators, FlexibleInstances #-}
 
 module Control.ECS.Types where
 
@@ -8,51 +8,42 @@ import Control.Lens
 
 newtype Entity = Entity Int
 
-class Component comp where
+class Monad m => Component m c where
 
-  type Repr    comp :: *
-  type Storage comp :: *
+  type Repr    c :: *
+  type Storage c :: *
 
-  empty    :: Store comp
-  slice    :: System (Store comp) (Slice comp)
-  retrieve :: Entity -> System (Store comp) (Reads comp)
-  store    :: Entity -> Writes comp -> System (Store comp) ()
+  empty    :: m (Store c)
+  slice    :: System (Store c) m (Slice c)
+  retrieve :: Entity -> System (Store c) m (Reads c)
+  store    :: Entity -> Writes c -> System (Store c) m ()
 
-newtype System s a = System {runSystem :: StateT s IO a}
-deriving instance Functor      (System s)
-deriving instance Applicative  (System s)
-deriving instance Monad        (System s)
-deriving instance MonadIO      (System s)
-deriving instance MonadState s (System s)
+type System = StateT
+
+runSystem :: System s m a -> s -> m (a, s)
+runSystem = runStateT
 
 newtype Slice  comp = Slice S.IntSet
 newtype Reads  comp = Reads (Repr comp)
 newtype Writes comp = Writes (Repr comp)
 newtype Store  comp = Store { _unStore :: Storage comp }
-makeLenses ''Store
+makeLenses  ''Store
 makeWrapped ''Store
 
-runSystem' :: System s a -> s -> System c (a, s)
-runSystem' sys = liftIO . runStateT (runSystem sys)
+instance (Monad (t m), Component m a, MonadTrans t) => Component (t m) a where
 
-instance (Component a, Component b) => Component (a, b) where
+instance (Component m a, Component m b) => Component m (a, b) where
 
   type Repr    (a, b) = (Repr a, Repr b)
   type Storage (a, b) = (Storage a, Storage b)
 
   empty =
-    let Store sta :: Store a = empty
-        Store stb :: Store b = empty
-     in Store (sta, stb)
+    do Store sta :: Store a <- empty
+       Store stb :: Store b <- empty
+       return $ Store (sta, stb)
 
   slice =
     do Store (sta, stb) <- get
-       (Slice sla, Store sta') <- runSystem' slice (Store sta :: Store a)
-       (Slice slb, Store stb') <- liftIO . runStateT (runSystem slice) $ (Store stb :: Store b)
-       put (Store (sta', stb'))
-       return $ Slice (sla `S.intersection` slb)
-
-  retrieve ety =
-    do Store (sta, stb) <- get
-       (Reads ra, Store sta') <- liftIO . runStateT (runSystem $ retrieve ety) $ (Store sta :: Store a)
+       (Slice sla, Store sta') <- runSystem slice (Store sta :: Store a)
+       (Slice slb, Store stb') <- runSystem slice (Store stb :: Store b)
        undefined
