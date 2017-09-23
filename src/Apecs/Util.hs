e769d6f9a9ffe12939b4dedabe387b426b7bd7c8
Jonas Carpay <jonascarpay@gmail.com>
2017-09-23 07:11:39 +0200

    better spatial hashing documentation

--- src/Apecs/Util.hs
+++ src/Apecs/Util.hs
@@ -11,7 +11,7 @@ module Apecs.Util (
 
   -- * Spatial hashing
   -- $hash
-  quantize, flatten, region, inbounds,
+  quantize, flatten, inbounds, region, unsafeFlatten,
 
   -- * Timing
   timeSystem, timeSystem_,
@@ -65,21 +65,41 @@ runGC = liftIO performMajorGC
 -- $hash
 -- The following functions are for spatial hashing.
 -- The idea is that your spatial hash is defined by two vectors;
+--
 --   - The cell size vector contains real components and dictates
---     how large each cell in your table is spatially.
---     It is used to translate from world-space to table space
---   - The field size vector contains integral components and dictates how
+--     how large each cell in your table is in world space units.
+--     It is used by @quantize@ to translate a world space coordinate into a table space index vector
+--   - The table size vector contains integral components and dictates how
 --     many cells your field consists of in each direction.
---     It is used to translate from table-space to a flat integer
+--     It is used by @flatten@ to translate a table-space index vector into a flat integer
+--
+-- There is currently no dedicated spatial hashing log, but you can use
+-- an EnumTable by defining an instance Enum Vec with
+-- > fromEnum = flatten size . quantize cell
 
 -- | Quantize turns a world-space coordinate into a table-space coordinate by dividing
---   by the given cell size and round components towards negative infinity
+--   by the given cell size and rounding towards negative infinity.
 {-# INLINE quantize #-}
 quantize :: (Fractional (v a), Integral b, RealFrac a, Functor v)
          => v a -- ^ Quantization cell size
          -> v a -- ^ Vector to be quantized
          -> v b
 quantize cell vec = floor <$> vec/cell
+
+-- | Turns a table-space vector into an integral index, given some table size vector.
+--   Yields Nothing for out-of-bounds queries
+{-# INLINE flatten #-}
+flatten :: (Applicative v, Integral a, Foldable v)
+        => v a -- Field size vector
+        -> v a -> Maybe a
+flatten size vec = if inbounds size vec then Just (unsafeFlatten size vec) else Nothing
+
+-- | Tests whether a vector is in the region given by 0 and the size vector (inclusive)
+{-# INLINE inbounds #-}
+inbounds :: (Num a, Ord a, Applicative v, Foldable v)
+         => v a -- Field size vector
+         -> v a -> Bool
+inbounds size vec = and (liftA2 (\v s -> v >= 0 && v <= s) vec size)
 
 -- | For two table-space vectors indicating a region's bounds, gives a list of the vectors contained between them.
 --   This is useful for querying a spatial hash.
@@ -90,19 +110,12 @@ region :: (Enum a, Applicative v, Traversable v)
        -> [v a]
 region a b = sequence $ liftA2 enumFromTo a b
 
--- | Turns a table-space vector into a linear index, given some table size vector.
-{-# INLINE flatten #-}
-flatten :: (Applicative v, Integral a, Foldable v)
-        => v a -- Field size vector
-        -> v a -> a
-flatten size vec = foldr (\(n,x) acc -> n*acc + x) 0 (liftA2 (,) size vec)
-
--- | Tests whether a vector is in the region given by 0 and the size vector
-{-# INLINE inbounds #-}
-inbounds :: (Num (v a), Ord a, Applicative v, Foldable v)
-         => v a -> v a -> Bool
-inbounds size vec = and (liftA2 (>=) vec 0) && and (liftA2 (<=) vec size)
-
+-- | Unsafe version of flatten. Yields garbage for out-of-bounds queries.
+{-# INLINE unsafeFlatten #-}
+unsafeFlatten :: (Applicative v, Integral a, Foldable v)
+              => v a -- Field size vector
+              -> v a -> a
+unsafeFlatten size vec = foldr (\(n,x) acc -> n*acc + x) 0 (liftA2 (,) size vec)
 
 -- | Runs a system and gives its execution time in seconds
 {-# INLINE timeSystem #-}
