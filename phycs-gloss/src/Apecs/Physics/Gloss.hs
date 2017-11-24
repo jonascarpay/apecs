deed3613d930fe3b8c043f45300525561e07cbc0
Jonas Carpay <jonascarpay@gmail.com>
2017-11-25 00:35:28 +0100

    GlossView functions

--- phycs-gloss/src/Apecs/Physics/Gloss.hs
+++ phycs-gloss/src/Apecs/Physics/Gloss.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE TypeFamilies               #-}
+{-# LANGUAGE ViewPatterns               #-}
 
 module Apecs.Physics.Gloss
   ( fromShape, drawWorld, BodyPicture(..),
@@ -17,6 +18,15 @@ newtype BodyPicture = BodyPicture G.Picture deriving Monoid
 instance Component BodyPicture where
   type Storage BodyPicture = Map BodyPicture
 
+data GlossView = GlossView (V2 Double) Double
+
+applyView :: GlossView -> G.Picture -> G.Picture
+applyView (GlossView (V2 (realToFrac -> x) (realToFrac -> y)) (realToFrac -> scale)) =
+  G.Translate x y . G.Scale scale scale
+
+mouseToWorld :: GlossView -> V2 Double -> V2 Double
+mouseToWorld (GlossView offset scale) mvec = (/scale) <$> mvec-offset
+
 fromShape :: ShapeType -> G.Picture
 fromShape (Circle (V2 x y) radius) = G.Translate (realToFrac x) (realToFrac y) $ G.Circle (realToFrac radius)
 fromShape (Segment (a,b) _) = G.Line [v2ToTuple a, v2ToTuple b]
@@ -24,6 +34,7 @@ fromShape (Convex verts _) = G.Polygon (v2ToTuple <$> verts)
 
 v2ToTuple :: V2 Double -> (Float, Float)
 v2ToTuple (V2 x y) = (realToFrac x, realToFrac y)
+
 
 drawWorld :: (Has w Physics, Has w BodyPicture) => System w G.Picture
 drawWorld = fmap fold . cmapM $ \((Position (V2 x y), Angle theta, BodyPicture pic)) ->
