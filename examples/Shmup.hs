ffc42e500e50fd1a642c225f3035d175e6eed445
Jonas Carpay <jonascarpay@gmail.com>
2018-07-09 08:21:31 +0200

    Fix example

--- examples/Shmup.hs
+++ examples/Shmup.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE FlexibleContexts, DataKinds, ScopedTypeVariables, TypeApplications, TypeFamilies, MultiParamTypeClasses, TemplateHaskell #-}
 
 import Apecs
-import Data.Proxy
+import Data.Monoid
 import Control.Monad
 import Linear
 import Graphics.Gloss
@@ -37,79 +37,91 @@ instance Component Particle where type Storage Particle = Map Particle
 makeWorld "World" [''Position, ''Velocity, ''Player, ''Target, ''Bullet, ''Score, ''Time, ''Particle]
 
 type System' a = System World a
-type Movable = (Position, Velocity)
+type Kinetic = (Position, Velocity)
 
+playerSpeed, bulletSpeed, enemySpeed, xmin, xmax :: Double
 playerSpeed = 170
-playerPos = V2 0 (-120)
-scorePos  = V2 xmin (-170)
 bulletSpeed = 500
 enemySpeed  = 80
 xmin = -100
 xmax = 100
 
-spawnParticles n pos color dvx dvy = replicateM_ n $ do
-  vx <- liftIO $ randomRIO dvx
-  vy <- liftIO $ randomRIO dvy
-  t  <- liftIO $ randomRIO (0.02,0.3)
-  newEntity (Particle color t, Position pos, Velocity (V2 vx vy))
+hitBonus, missPenalty :: Int
+hitBonus = 100
+missPenalty = 40
+
+playerPos, scorePos :: V2 Double
+playerPos = V2 0 (-120)
+scorePos  = V2 xmin (-170)
 
 initialize :: System' ()
 initialize = void $ newEntity (Player, Position playerPos, Velocity 0)
 
+stepPosition :: Double -> System' ()
+stepPosition dT = cmap $ \(Position p, Velocity v) -> Position (p + dT *^ v)
+
+clampPlayer :: System' ()
+clampPlayer = cmap $ \(Player, Position (V2 x y))
+                   -> Position (V2 (min xmax . max xmin $ x) y)
+
+incrTime :: Double -> System' ()
+incrTime dT = modify global $ \(Time t) -> Time (t+dT)
+
+clearTargets :: System' ()
+clearTargets = cmap $ \all@(Target, Position (V2 x _), Velocity _) ->
+  if x < xmin || x > xmax
+     then Nothing
+     else Just all
+
+stepParticles :: Double -> System' ()
+stepParticles dT = cmap $ \(Particle col t) ->
+  if t < 0
+     then Right $ Not @(Particle, Kinetic)
+     else Left  $ Particle col (t-dT)
+
+clearBullets :: System' ()
+clearBullets = cmap $ \(Bullet, Position (V2 x y), Score s) ->
+  if y > 170
+     then Right $ (Not @(Bullet, Kinetic), Score (s-missPenalty))
+     else Left ()
+
+triggerEvery :: Double -> Double -> Double -> System' a -> System' ()
+triggerEvery dT period phase sys = do
+  Time t <- get global
+  let t' = t + phase
+      trigger = floor (t'/period) /= floor ((t'+dT)/period)
+  when trigger $ void sys
+
+spawnParticles :: Int -> Position -> Color -> (Double,Double) -> (Double,Double) -> System' ()
+spawnParticles n pos color dvx dvy = replicateM_ n $ do
+  vx <- liftIO $ randomRIO dvx
+  vy <- liftIO $ randomRIO dvy
+  t  <- liftIO $ randomRIO (0.02,0.3)
+  newEntity (Particle color t, pos, Velocity (V2 vx vy))
+
+handleCollisions :: System' ()
+handleCollisions =
+  cmapM_ $ \(Target, Position posT, etyT) ->
+    cmapM_ $ \(Bullet, Position posB, etyB) ->
+      when (norm (posT - posB) < 10) $ do
+        destroy etyT (Proxy @(Target, Kinetic))
+        destroy etyB (Proxy @(Bullet, Kinetic))
+        spawnParticles 15 (Position posB) white (-500,500) (200,-50)
+        modify global $ \(Score x) -> Score (x + hitBonus)
+
 step :: Double -> System' ()
 step dT = do
-  incrTime
-  stepPosition
+  incrTime dT
+  stepPosition dT
   clampPlayer
   clearTargets
   clearBullets
-  stepParticles
+  stepParticles dT
   handleCollisions
-  triggerEvery 0.6 0   $ newEntity (Target, Position (V2 xmin 80), Velocity (V2 enemySpeed 0))
-  triggerEvery 0.6 0.3 $ newEntity (Target, Position (V2 xmax 120), Velocity (V2 (negate enemySpeed) 0))
-  where
-    incrTime = modify 0 $ \(Time t) -> Time (t+dT)
-    stepPosition = cmap $ \(Position p, Velocity v) -> Position (p + dT *^ v)
-    clampPlayer = cmap $ \(Player, Position (V2 x y)) -> Position (V2 (min xmax . max xmin $ x) y)
-    triggerEvery period phase sys = do
-      Time t <- get global
-      let t' = t + phase
-      when (floor (t'/period) /= floor ((t'+dT)/period)) (void sys)
-    clearBullets = cmapM_ $ \(Bullet, Position (V2 x y), ety) ->
-      when (y > 170) $ do
-        destroy ety (Proxy @(Bullet, Movable))
-        modify global $ \(Score x) -> Score (x-40)
-    clearTargets = cmap $ \c@(Target, Position (V2 x y), Velocity v) ->
-      if x < xmin || x > xmax
-         then Nothing
-         else Just c
-    handleCollisions =
-      cmapM_ $ \t@(Target, Position pTarget, Velocity _, eTarget) ->
-        cmapM_ $ \b@(Bullet, Position pBullet, Velocity _, eBullet) ->
-          when (norm (pTarget - pBullet) < 10) $ do
-            destroy eTarget (Proxy @(Target, Position, Velocity))
-            destroy eBullet (Proxy @(Bullet, Position, Velocity))
-            spawnParticles 15 pBullet white (-500,500) (200,-50)
-            modify global $ \(Score x) -> Score (x + 100)
-    stepParticles = cmapM_ $ \(Particle col t, ety) ->
-      if t < 0
-         then destroy ety (Proxy @(Particle, Movable))
-         else set ety (Particle col (t-dT))
+  triggerEvery dT 0.6 0   $ newEntity (Target, Position (V2 xmin 80), Velocity (V2 enemySpeed 0))
+  triggerEvery dT 0.6 0.3 $ newEntity (Target, Position (V2 xmax 120), Velocity (V2 (negate enemySpeed) 0))
 
-draw :: System' Picture
-draw = do
-  p <- toPic $ \(Player, Position p) -> color white  . translate' p . scale 10 20 $ triangle
-  t <- toPic $ \(Target, Position p) -> color red    . translate' p . scale 10 10 $ diamond
-  b <- toPic $ \(Bullet, Position p) -> color yellow . translate' p . scale 4  4   $ diamond
-  s <- flip fmap (get global) $ \(Score s) -> color white . translate' scorePos . scale 0.1 0.1 . Text $ "Score: " ++ show s
-  pt <- toPic $ \(Particle col _, Position p, Velocity (V2 vx vy)) -> color col . translate' p $ Line [(0,0),(realToFrac vx/10, realToFrac vy/10)]
-  return $ mconcat [p,t,b,s,pt]
-  where
-    toPic f = mconcat . fmap f <$> getAll
-    translate' (V2 x y) = translate (realToFrac x) (realToFrac y)
-    triangle = Line [(0,0),(-0.5,-1),(0.5,-1),(0,0)]
-    diamond  = Line [(-1,0),(0,-1),(1,0),(0,1),(-1,0)]
-
+handleEvent :: Event -> System' ()
 handleEvent (EventKey (SpecialKey KeyLeft) Down _ _) =
   cmap $ \(Player, Velocity (V2 x _)) -> Velocity (V2 (x-playerSpeed) 0)
 
@@ -123,11 +135,39 @@ handleEvent (EventKey (SpecialKey KeyRight) Up   _ _) =
   cmap $ \(Player, Velocity (V2 x _)) -> Velocity (V2 (x-playerSpeed) 0)
 
 handleEvent (EventKey (SpecialKey KeySpace) Down _ _) =
-  cmapM_ $ \(Player, Position p) -> do
-    newEntity (Bullet, Position p, Velocity (V2 0 bulletSpeed))
-    spawnParticles 7 p yellow (-80,80) (10,100)
+  cmapM_ $ \(Player, pos) -> do
+    newEntity (Bullet, pos, Velocity (V2 0 bulletSpeed))
+    spawnParticles 7 pos yellow (-80,80) (10,100)
 
 handleEvent _ = return ()
+
+drawComponents :: Get World c => (c -> Picture) -> System' Picture
+drawComponents f = cfold
+  (\pic (Position p, c) -> pic <> translate' p (f c))
+  mempty
+
+translate' :: V2 Double -> Picture -> Picture
+translate' (V2 x y) = translate (realToFrac x) (realToFrac y)
+
+triangle, diamond :: Picture
+triangle = Line [(0,0),(-0.5,-1),(0.5,-1),(0,0)]
+diamond  = Line [(-1,0),(0,-1),(1,0),(0,1),(-1,0)]
+
+draw :: System' Picture
+draw = do
+  player  <- drawComponents $ \Player -> color white  . scale 10 20 $ triangle
+  targets <- drawComponents $ \Target -> color red    . scale 10 10 $ diamond
+  bullets <- drawComponents $ \Bullet -> color yellow . scale 4  4  $ diamond
+
+  particles <- drawComponents $
+    \(Particle col _, Velocity (V2 vx vy))
+    -> color col $ Line [(0,0),(realToFrac vx/10, realToFrac vy/10)]
+
+  Score s <- get global
+  let score = color white . translate' scorePos . scale 0.1 0.1 . Text $ "Score: " ++ show s
+
+  return $ player <> targets <> bullets <> score <> particles
+  where
 
 playGloss :: w
           -> System w Picture
@@ -144,6 +184,7 @@ playGloss world drawSys eventSys stepSys =
     window = InWindow "game" (220,360) (10,10)
     fps = 60
 
+main :: IO ()
 main = do
   w <- initWorld
   runSystem initialize w
