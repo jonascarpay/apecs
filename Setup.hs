812d2bc60b2c6c266ac232bc1a8e7f01248f6a7f
Gaël Deest <gael.deest@tweag.io>
2018-10-02 09:01:22 +0200

    Custom Setup.hs hook for inline-c backward compatibility
    inline-c < 0.6.0 generates C files in source directories that must be added to the list of c-sources.
    
    inline-c < 0.6.0 generates C files in source directories that must be added to the list of c-sources.

--- Setup.hs
+++ Setup.hs
@@ -1,2 +1,40 @@
+{-# LANGUAGE CPP #-}
 import Distribution.Simple
-main = defaultMain
+import Distribution.PackageDescription
+
+
+main = defaultMainWithHooks simpleUserHooks
+    { readDesc = (fmap . fmap) addCFilesIfNeeded (readDesc simpleUserHooks)
+    }
+  where
+    addCFilesIfNeeded genPkgDescr =
+#if MIN_VERSION_inline_c(0, 6, 0)
+      genPkgDescr
+#else
+      addCFiles genPkgDescr
+#endif
+
+    addCFiles genPkgDescr = genPkgDescr
+      { packageDescription = newPkgDescription
+      }
+      where
+        pkgDescr = packageDescription genPkgDescr
+        pkgLibrary = library pkgDescr
+        newPkgDescription = pkgDescr
+          { library = fmap addCFiles' pkgLibrary
+          }
+        addCFiles' lib = lib
+          { libBuildInfo =
+              let buildInfo = libBuildInfo lib in
+                buildInfo
+                { cSources = cSources buildInfo ++
+                    [ "src/Apecs/Physics/Constraint.c"
+                    , "src/Apecs/Physics/Space.c"
+                    , "src/Apecs/Physics/Query.c"
+                    , "src/Apecs/Physics/Body.c"
+                    , "src/Apecs/Physics/Collision.c"
+                    , "src/Apecs/Physics/Shape.c"
+                    ]
+                }
+          }
+
